4d110bcbc5f3989937728e7ed4cd73f9e7bd8b73
santiago <aguerosan@gmail.com>
2023-06-25 20:31:58 -0400

    search & save temp products by form

--- php/productos_detalle_temp.php
+++ php/productos_detalle_temp.php
@@ -13,7 +13,7 @@ if($cantidad == "" || is_numeric($cantidad) == false){
         <strong>¡Ocurrió un error inesperado!</strong><br>
         Debes ingresar una cantidad.
     </div>';
-
+    exit();
 }
 
 //verifica integridad de los datos
@@ -23,6 +23,7 @@ if(verificar_datos("[1-9]{1,3}",$cantidad)){
         <strong>¡Ocurrió un error inesperado!</strong><br>
         Cantidad no admitida.
     </div>';
+    exit();
 }
 
 //verifica producto id
@@ -35,7 +36,36 @@ if($check_id->rowCount()<=0){
         <strong>¡Ocurrió un error inesperado!</strong><br>
         El producto que intenta agregar no existe.
     </div>';
+    exit();
 }
 $check_id=null;
 
 echo $id;
+
+//guardando datos
+$guardar_temp = con();
+//prepare: prepara la consulta antes de insertar directo a la bd. variables sin comillas ni $
+$guardar_temp = $guardar_temp->prepare("INSERT INTO
+    detalle_temp(producto_id, cantidad, precio_venta)
+    VALUES(:id, :cantidad, :precio)");
+
+//evitando inyecciones sql xss
+$marcadores=[
+    ":id"=>$id, ":cantidad"=>$cantidad, ":precio"=>$precio];
+
+$guardar_temp->execute($marcadores);
+
+if($guardar_temp->rowCount()==1){// 1 usuario nuevo insertado
+    echo '
+    <div class="notification is-success is-light">
+        <strong>DEtalle Temporal registrado!</strong><br>
+        El detalle temporal se registró exitosamente.
+    </div>';
+} else {
+    echo '
+    <div class="notification is-danger is-light">
+        <strong>¡Ocurrió un error inesperado!</strong><br>
+        No se pudo detalle temporal , inténtelo nuevamente.
+    </div>';
+}
+$guardar_temp=null; //cerrar conexion;
